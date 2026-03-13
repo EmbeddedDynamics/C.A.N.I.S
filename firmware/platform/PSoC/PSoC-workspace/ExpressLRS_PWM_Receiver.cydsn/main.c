6f822ddd2d909ec36f3af3a7d3b448a48ac38870
Daan Smit <daan26smit@gmail.com>
2026-03-13 12:55:28 +0100

    Now reading PWM pulses correctly
    Changed how interrupt service is working. Counter was counting down
    instead of counting up which caused weird values

--- firmware/platform/PSoC/PSoC-workspace/ExpressLRS_PWM_Receiver.cydsn/main.c
+++ firmware/platform/PSoC/PSoC-workspace/ExpressLRS_PWM_Receiver.cydsn/main.c
@@ -15,6 +15,7 @@
 //========================================================================================================================//
 
 #include "project.h"
+#include <stdbool.h>
 
 //========================================================================================================================//
 //                                                 Global Variabeles                                                      //                                                                 
@@ -23,63 +24,13 @@
 volatile uint32_t CH1_rising_edge_start = 0, CH2_rising_edge_start = 0, CH3_rising_edge_start = 0, CH4_rising_edge_start = 0;
 volatile uint32_t CH1_Width = 0, CH2_Width = 0, CH3_Width = 0, CH4_Width = 0;
 
-//=========================================================================================================================//
-//                                                 Function Prototypes                                                     //                                                                 
-//======================================================================================================================== //
 
-uint32_t getRadioPWM(int CH_number);
+CY_ISR_PROTO(CH1_Handler);
+CY_ISR_PROTO(CH2_Handler);
+CY_ISR_PROTO(CH3_Handler);
+CY_ISR_PROTO(CH4_Handler);
 
-//========================================================================================================================//
-//                                                 INTERRUPT HANDLERS                                                     //                                                                 
-//========================================================================================================================//
-
-CY_ISR(isr_1_Handler) {
-    uint32_t state = CH1_Pin_Read();
-    if(state == 1) {
-        CH1_rising_edge_start = CH1_Timer_ReadCapture();
-    }
-    else {
-        CH1_Width = CH1_Timer_ReadCapture() - CH1_rising_edge_start;
-        CH1_Width = CH1_Width * 1000/24; // 24 MHz clock to microseconds
-    }
-}
-
-CY_ISR(isr_2_Handler) {
-    uint32_t state = CH2_Pin_Read();
-    if(state == 1) {
-        CH2_rising_edge_start = CH2_Timer_ReadCapture();
-    }
-    else {
-        CH2_Width = CH2_Timer_ReadCapture() - CH2_rising_edge_start;
-        CH2_Width = CH2_Width * 1000/2; // 24 MHz clock to microseconds
-    }
-}
-
-CY_ISR(isr_3_Handler) {
-    uint32_t state = CH3_Pin_Read();
-    if(state == 1) {
-        CH3_rising_edge_start = CH3_Timer_ReadCapture();
-    }
-    else {
-        CH3_Width = CH3_Timer_ReadCapture() - CH3_rising_edge_start;
-        CH3_Width = CH3_Width * 1000/24; // 24 MHz clock to microseconds
-    }
-}
-
-CY_ISR(isr_4_Handler) {
-    uint32_t state = CH4_Pin_Read();
-    if(state == 1) {
-        CH4_rising_edge_start = CH4_Timer_ReadCapture();
-    }
-    else {
-        CH4_Width = CH4_Timer_ReadCapture() - CH4_rising_edge_start;
-        CH4_Width = CH4_Width * 1000/24; // 24 MHz clock to microseconds
-    }
-}
-
-//=========================================================================================================================//
-//                                                 For loop                                                                //                                                                 
-//======================================================================================================================== //
+uint32_t getRadioPWM(int CH_Number);
 
 int main(void)
 {
@@ -93,17 +44,18 @@ int main(void)
 
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
     
+    // Start all timers
     CH1_Timer_Start();
-    isr_1_StartEx(isr_1_Handler);
-    
     CH2_Timer_Start();
-    isr_2_StartEx(isr_2_Handler);
-    
     CH3_Timer_Start();
-    isr_3_StartEx(isr_3_Handler);
-    
     CH4_Timer_Start();
-    isr_4_StartEx(isr_4_Handler);
+    
+    // Start all interrupt handlers with custom names
+    isr_1_StartEx(CH1_Handler);
+    isr_2_StartEx(CH2_Handler);
+    isr_3_StartEx(CH3_Handler);
+    isr_4_StartEx(CH4_Handler);
+    
 
     //=========================================================================================================================//
     //                                                 Main Loop                                                               //                                                                 
@@ -111,30 +63,114 @@ int main(void)
     
     for(;;)
     {
-        /* Place your application code here. */
+        int32_t ch1 = getRadioPWM(1);
+        int32_t ch2 = getRadioPWM(2);
+        int32_t ch3 = getRadioPWM(3);
+        int32_t ch4 = getRadioPWM(4);
         
-        uint32_t CH1 = getRadioPWM(1);
-        uint32_t CH2 = getRadioPWM(2);
-        uint32_t CH3 = getRadioPWM(3);
-        uint32_t CH4 = getRadioPWM(4);
+        CyDelay(10);  // Small delay
+        if (ch3 > 1600) {
+            LED_BUILDIN_Write(1);
+        }
+        else if (ch3 < 1400) {
+            LED_BUILDIN_Write(1);
+        }
+        else {
+            LED_BUILDIN_Write(0);
+        }
     }
 }
 
+//=========================================================================================================================//
+//                                                 getRadioPWM function                                                    //                                                                 
+//======================================================================================================================== //
 
-uint32_t getRadioPWM (int CH_Number) {
+uint32_t getRadioPWM(int CH_Number) {
+    uint32_t returnPWM = 0;
     if(CH_Number == 1) {
-        return CH1_Width;
+        returnPWM = CH1_Width;
     }
-    else if (CH_Number == 2) {
-        return CH2_Width;
+    else if(CH_Number == 2) {
+        returnPWM = CH2_Width;
     }
-    else if (CH_Number == 3) {
-        return CH3_Width;
+    else if(CH_Number == 3) {
+        returnPWM = CH3_Width;
     }
-    else if (CH_Number == 4) {
-        return CH4_Width;
+    else if(CH_Number == 4) {
+        returnPWM = CH4_Width;
     }
+    return returnPWM;
 }
+
+//========================================================================================================================//
+//                                                 INTERRUPT HANDLERS                                                     //                                                                 
+//========================================================================================================================//
+
+
+CY_ISR(CH1_Handler) {
+    int state = CH1_Pin_Read();
+    uint32_t current_time = CH1_Timer_ReadCounter();
+    
+    if(state == 1) {
+        // Rising edge
+        CH1_rising_edge_start = current_time;
+    } 
+    else {
+        // Falling edge - DOWN counting timer
+        CH1_Width = CH1_rising_edge_start - current_time;
+    }
+    
+    CH1_Pin_ClearInterrupt();
+}
+
+CY_ISR(CH2_Handler) {
+    int state = CH2_Pin_Read();
+    uint32_t current_time = CH2_Timer_ReadCounter();
+    
+    if(state == 1) {
+        // Rising edge
+        CH2_rising_edge_start = current_time;
+    } 
+    else {
+        // Falling edge - DOWN counting timer
+        CH2_Width = CH2_rising_edge_start - current_time;
+    }
+    
+    CH2_Pin_ClearInterrupt();
+}
+
+CY_ISR(CH3_Handler) {
+    int state = CH3_Pin_Read();
+    uint32_t current_time = CH3_Timer_ReadCounter();
+    
+    if(state == 1) {
+        // Rising edge
+        CH3_rising_edge_start = current_time;
+    } 
+    else {
+        // Falling edge - DOWN counting timer
+        CH3_Width = CH3_rising_edge_start - current_time;
+    }
+    
+    CH3_Pin_ClearInterrupt();
+}
+
+CY_ISR(CH4_Handler) {
+    int state = CH4_Pin_Read();
+    uint32_t current_time = CH4_Timer_ReadCounter();
+    
+    if(state == 1) {
+        // Rising edge
+        CH4_rising_edge_start = current_time;
+    } 
+    else {
+        // Falling edge - DOWN counting timer
+        CH4_Width = CH4_rising_edge_start - current_time;
+    }
+    
+    CH4_Pin_ClearInterrupt();
+}
+
 
 
 /* [] END OF FILE */
